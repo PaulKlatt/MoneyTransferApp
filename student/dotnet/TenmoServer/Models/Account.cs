21a72b2428de813149f47438a790468023f0b391
Ryan Harris <rhtrumpet@gmail.com>
2021-11-03 17:13:43 -0400

    Get Balance functionality

--- student/dotnet/TenmoServer/Models/Account.cs
+++ student/dotnet/TenmoServer/Models/Account.cs
@@ -7,10 +7,12 @@ namespace TenmoServer.Models
 {
     public class Account
     {
-        public int AccountId { get; set; }
+        public int? AccountId { get; set; }
 
         public int UserId { get; set; }
 
         public decimal Balance { get; set; }
     }
+
+ 
 }
