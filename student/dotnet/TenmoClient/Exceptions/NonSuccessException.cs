61e67ce6f65755c62af6ca116298a9d5b0c88dfe
Jacqueline <Kerg>
2021-11-04 12:01:35 -0400

    cleaning account path and listing multiple accounts

---
+++ student/dotnet/TenmoClient/Exceptions/NonSuccessException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TenmoClient.Exceptions
+{
+    public class NonSuccessException : Exception
+    {
+        private const string NON_SUCCESS_MESSAGE = "Error occurred - received non-success response: ";
+
+        // default constructors
+        public NonSuccessException() : base() { }
+        public NonSuccessException(string message) : base(message) { }
+        public NonSuccessException(string message, Exception innerException) : base(message, innerException) { }
+
+        // custom constructors
+        public NonSuccessException(int statusCode) : base(NON_SUCCESS_MESSAGE + statusCode) { }
+
+        public NonSuccessException(int statusCode, Exception innerException) : base(NON_SUCCESS_MESSAGE + statusCode, innerException) { }
+    }
+}
--- student/dotnet/TenmoClient/Models/Account.cs
+++ student/dotnet/TenmoClient/Models/Account.cs
@@ -9,11 +9,7 @@ namespace TenmoServer.Models
     public class Account
     {
         public int? AccountId { get; set; }
-
-        [Required(ErrorMessage = "User ID field is required.")]
         public int UserId { get; set; }
-
-        [Range((double)0.00M, double.PositiveInfinity, ErrorMessage = "Balance is required.")]
         public decimal Balance { get; set; }
     }
 }
